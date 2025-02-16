217d220ba5721f19d8466138a2d62728a9ab8727
Zuli Amrullah <zulyazqall@gmail.com>
2025-02-16 16:25:06 +0900

    init

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var call = new Audio();
+call.src = "sounds/911.mp3";
+
+var chuckles = new Audio();
+chuckles.src = "sounds/girl-laugh-6689.mp3";
+
+var ambulance = new Audio();
+ambulance.src = "sounds/ambulance-6399.mp3";
+
+var collapse = new Audio();
+collapse.src = "sounds/collapse-47652.mp3";
+
+var walk = new Audio();
+walk.src = "sounds/walk.mp3";
+
+var check = new Audio();
+check.src = "sounds/hospital-monitor-151929.mp3";
+
+var door = new Audio();
+door.src = "sounds/hospital-door-latch-77569.mp3";
