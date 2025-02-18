d5326342dd0baf7ffcc27c8c66502f140607009f
Zuli Amrullah <zulyazqall@gmail.com>
2025-02-18 14:34:03 +0900

    add and update sound

--- js/script.js
+++ js/script.js
@@ -20,7 +20,16 @@ var door = new Audio();
 door.src = "sounds/hospital-door-latch-77569.mp3";
 
 var laugh = new Audio();
-laugh.src = "sounds/kids-laugh-45357";
+laugh.src = "sounds/kids-laugh-45357.mp3";
 
 var scream = new Audio();
-scream.src = "sounds/kids-screaming-47198";
+scream.src = "sounds/kids-screaming-47198.mp3";
+
+var knock = new Audio();
+knock.src = "sounds/knock-on-door-6228.mp3";
+
+var road = new Audio();
+road.src = "sounds/ambulance-in-city-traffic-139030.mp3";
+
+var out = new Audio();
+out.src = "sounds/happy-outro-8110.mp3";
